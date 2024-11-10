175202d7427932a66db74d9e63172320bf519286
Rituparna Kar <167243883+RitzKar@users.noreply.github.com>
2024-11-10 02:05:22 +0000

    git

--- app.py
+++ app.py
@@ -18,71 +18,19 @@ ad_type = st.radio("Which continent do you want to travel?",
 ['Americas', 'Australia', 'Europe', 'Asia', 'Africa'], horizontal=True)
 #st.write(f"You chose: {ad_type}")
 
-#Once the selection is made the corresponding tourist spots will be display
+#Once the selection is made the corresponding tourist spots will be display. 
+#To upload a csv file which contains all the tourist spots associated with a continent
 # Radio button with horizontal layout for ad_platform
-ad_platform = st.radio("What Ad platform do you want?",
-['Facebook', 'Google', 'Instagram', 'LinkedIn'], horizontal=True)
-#st.write(f"You chose: {ad_platform}")
-
-# Radio button with horizontal layout for ad_platform
-device_type = st.radio("What device type do you want?",
-['Mobile', 'Laptop', 'Tablet'], horizontal=True)
-#st.write(f"You chose: {ad_platform}")
 
 
 # Radio button with horizontal layout for ad_platform
-ad_placement = st.radio("What ad placement do you want?",
-['Banner', 'Sidebar', 'Pre-roll', 'Pop-up', 'In-feed','Sponsored content'], horizontal=True)
+activities = st.radio("What activities do you want?",
+['Sightseeing', 'swimming', 'water sports', 'spa', 'Kids theme parks', 'Landmarks', 'Fitness', 'Fashion'], horizontal=True)
 #st.write(f"You chose: {ad_platform}")
 
+st.subheader("Budget")
 
-# Radio button with horizontal layout for ad_platform
-ad_location = st.radio("What ad location do you want?",
-['Upper Left', 'Lower Left', 'Upper right', 'Lower right', 'Center','Upper Centre', 'Lower Centre'], horizontal=True)
-#st.write(f"You chose: {ad_platform}")
-
-
-st.subheader("Target Audience")
-
-age = st.slider('What age are you targetting?', 0, 100, 25)
-
-gender = st.radio(
-    "What gender are you targetting?",
-    ('Male', 'Female', 'Non-binary'))
-
-income = st.slider('What income range are you targetting?', 0, 100000, 50000)
-
-location = st.text_input('What location are you targetting?')
-
-audience_interest = st.radio("What audience interest are you targetting?",
-['Technology', 'Fitness', 'Travel', 'Fashion', 'Movies'], horizontal=True)
-#st.write(f"You chose: {ad_platform}")
-
-ad_objective = st.radio("What audience interest are you targetting?",
-['Brand Awareness', 'Lead generation', 'App installs', 'Sales conversion'], horizontal=True)
-
-bidding_strategy = st.radio("What is your bidding strategy?",
-['Cost per click', 'Cost per thousand impressions', 'Cost per acquisition'], horizontal=True)
-
-
-language = st.radio("What is ad language?",
-['English', 'Spanish','Hindi', 'Mandarin', 'French'], horizontal=True)
-
-
-time_of_day = st.radio("What time of the day do you want?",
-['Morning', 'Afternoon','Evening', 'Night'], horizontal=True)
-
-
-ad_dimensions = st.radio("Dimensions of the ad?",
-['300X250', '728X90','160X600'], horizontal=True)
-
-
-ad_network = st.radio("What is the ad network",
-['Google Display Network', 'Facebook audience network', 'Adsense'], horizontal=True)
-
-#def generate_response(input_text):
-  # model = ChatOpenAI(temperature=0.7, api_key=openai_api_key)
-    #st.info(model.invoke(input_text))
+budget = st.slider('How much are you willing to pay per night?', 100, 10000, 200)
 
 
 # Submit button
