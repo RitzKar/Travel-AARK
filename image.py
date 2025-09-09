0931b65d03ba9b491f7f39967b1840d10ba1f14e
andykar0912 <anindya.kar.tcs@gmail.com>
2025-09-09 20:32:03 +0000

    corrected the indentations in image.py

--- image.py
+++ image.py
@@ -2,16 +2,7 @@ import os
 import streamlit as st
 from openai import OpenAI
 
-def image_loader(city):
-image_dir = "images"  # your image folder
-image_file = f"{city}"  # replace with your image filename
-
-image_path = os.path.join(image_dir, image_file)
-if os.path.exists(image_path):
-    st.image(image_path, caption=image_file)
-elif not os.path.exists(image_dir):
-
-    def generate_image(prompt, api_key):
+def generate_image():
     client = openai.OpenAI(api_key=openai_api_key)
     response = client.images.generate(
         model="dall-e-3",
@@ -22,6 +13,17 @@ elif not os.path.exists(image_dir):
     image_url = response.data[0].url
     st.image(image_url, caption=prompt)
     return image_url
+
+def image_loader(city):
+    image_dir = "images"  # your image folder
+    image_file = f"{city}"  # replace with your image filename
+
+    image_path = os.path.join(image_dir, image_file)
+    if os.path.exists(image_path):
+        st.image(image_path, caption=image_file)
+    elif not os.path.exists(image_dir):
+        generate_image()
+        
 #else:
     #st.warning(f"Image {image_file} not found in {image_dir}.")
 
